50a6f91ff50b86e921f0784ed620f424445bcc49
Velenoks <pavel.zakharov.1994@yandex.ru>
2021-01-13 22:00:10 +0400

    Отредактировал модели

--- users/models.py
+++ users/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 
 #class User(AbstractUser):
-   # bio = models.TextField(max_length=300)
+   #bio = models.TextField(max_length=300)
     #USER_ROLES = ['admin', 'moderator', 'user']
     #role = models.CharField(blank=False, choices=USER_ROLES, default='user')
