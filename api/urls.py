9e50f6f2b9ec18fe0af2e85fb4e54f8f28b21656
Velenoks <pavel.zakharov.1994@yandex.ru>
2021-01-17 22:42:38 +0400

    30(31) tests complete

--- api/urls.py
+++ api/urls.py
@@ -29,9 +29,5 @@ router.register(r'titles/(?P<title_id>\d+)/'
                 basename=app_name)
 
 urlpatterns = [
-    path('v1/token/', TokenObtainPairView.as_view(),
-         name='token_obtain_pair'),
-    path('v1/token/refresh/', TokenRefreshView.as_view(),
-         name='token_refresh'),
     path('v1/', include(router.urls)),
 ]
